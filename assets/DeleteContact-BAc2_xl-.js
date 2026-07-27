0f7e1ae1b2f9a48a9356d4697df8f233110ad8b7
Rakesh-1143 <rakeshreddy261002@gmail.com>
2026-07-27 17:38:46 +0530

    Updates

---
+++ assets/DeleteContact-BAc2_xl-.js
@@ -0,0 +1,1 @@
+import{c as e,b as t}from"./index-BVIF5q0Z.js";e().shape({confirmDelete:t().oneOf([!0],"You must confirm to delete this contact")});
--- assets/DeleteContact-BHBG6u82.js
+++
@@ -1,1 +0,0 @@
-import{c as e,b as t}from"./index-DiQC-CY0.js";e().shape({confirmDelete:t().oneOf([!0],"You must confirm to delete this contact")});
